8d9d8456b58c6f866d04bebb4bf0e5c786e4621f
yancy <yancy22737@gmail.com>
2026-05-07 01:42:04 +0800

    auto-read version from assembly instead of hardcoding

--- FactionGearModification/Core/ModVersion.cs
+++ FactionGearModification/Core/ModVersion.cs
@@ -1,17 +1,26 @@
+using System.Reflection;
+
 namespace FactionGearCustomizer
 {
     public static class ModVersion
     {
-        private const string CurrentVersion = "v1.5.3";
+        private static string _cachedVersion;
 
         public static string Current
         {
             get
             {
-                return CurrentVersion;
+                if (_cachedVersion == null)
+                {
+                    var attr = Assembly.GetExecutingAssembly()
+                        .GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+                    string ver = attr?.InformationalVersion ?? "unknown";
+                    _cachedVersion = ver.StartsWith("v") ? ver : "v" + ver;
+                }
+                return _cachedVersion;
             }
         }
-        
+
         public static string GetChangelog()
         {
             return $"{LanguageManager.Get("Version")}: {Current}\n\n{LanguageManager.Get("Changelog")}:\n- Fix: {LanguageManager.Get("Changelog_FixWikiLang")}\n- Add: {LanguageManager.Get("Changelog_AddWikiUI")}";
